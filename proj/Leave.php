8c65d669b4253aeacfe45310cbe6e831ce249d30
glassresearch <sahaarpita0005@gmail.com>
2017-07-22 00:49:40 +0530

    view

--- proj/Leave.php
+++ proj/Leave.php
@@ -11,34 +11,57 @@
   <script>
   $( function() {
     $( "#date1" ).datepicker({
-	changeMonth: true,
-	changeYear: true,
-	duration:"slow"
+	showAnim: 'slideDown',
+		numberOfMonths:1,
+		minDate: '-3M',
+		dateFormat: 'yy-mm-dd',
+		changeMonth: true,
+	    changeYear: true,
+	    duration:"slow",
+		onClose: function( selectedDate ) {
+			$('#date2').datepicker("option","minDate",selectedDate);
+		}
     });	 
   }
 );
+$( function() {
+    $( "#date2" ).datepicker({
+		showAnim:'slideDown',
+		numberOfMonths:1,
+		dateFormat: 'yy-mm-dd',
+		changeMonth: true,
+	    changeYear: true,
+	    duration:"slow",
+		onClose: function( selectedDate ) {
+			$('#date1').datepicker("option","maxDate",selectedDate);
+	    }		
+	});
+	});
+
   </script>
 </head>
 <body>
  
         <marquee><h2>Enter your leave details.....!!!</h2></marquee>
- 	<form action="Leave.php" method = "post">
+ 	<form action="test.php" method = "post">
 
  
  Leave Type	<select name="type">
 			<option value=""> </option>
             <option value="sick">Sick</option>
-			<option value="privilege">Privilege</option
-			value="<?php echo isset($_POST['type']) ? $_POST['type'] : '' ?>">
+			<option value="privilege">Privilege</option>
+			value="<?php echo isset($_POST['type']) ? $_POST['type'] : '' ?>"
   
 			</select>
- <p>Start Date: <input type="text" id="date1" name="date1" value="<?php echo isset($_POST['date1']) ? $_POST['date1'] : '' ?>">></p>			
- Duration <input type="text" name="duration" id="duration" value="<?php echo isset($_POST['duration']) ? $_POST['duration'] : '' ?>">>	</p>
- Remarks <input type="text" name="remarks" id="remarks" value="<?php echo isset($_POST['remarks']) ? $_POST['remarks'] : '' ?>">> </p>
+Hours	<input type="text" name="hours" id="hours" value="<?php echo isset($_POST['hours']) ? $_POST['hours'] : '' ?>"></p>			
+<p>BB No.: <input type="text" name="username1" id="username1" value="<?php echo isset($_POST['username1']) ? $_POST['username1'] : '' ?>"></p>			
+ <p>Start Date: <input type="text" id="date1" name="date1" value="<?php echo isset($_POST['date1']) ? $_POST['date1'] : '' ?>"></p>			
+ <p>End Date: <input type="text" id="date2" name="date2" value="<?php echo isset($_POST['date2']) ? $_POST['date2'] : '' ?>"></p>			
+ Remarks <input type="text" name="remarks" id="remarks" value="<?php echo isset($_POST['remarks']) ? $_POST['remarks'] : '' ?>"> </p>
  	<input name = "Submit" type="submit" value="Submit" id ="Submit">
 	<input name = "Logout" type="submit" value="Logout" id ="Logout" formaction="login.php">
 
-
+</form>
  
  
 </body>
@@ -46,7 +69,7 @@
 
 
 <?php
-session_start();
+//session_start();
 $servername = "localhost";
 $dbname = "arpi_database";
 $username = "root";
@@ -63,22 +86,27 @@ echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 if(isset($_POST["Submit"]))  {
-$bb_no = $_SESSION['name'];	
+
 $type= $_POST['type']; 
-$duration= $_POST['duration']; 
-$date1= strtotime($_POST['date1']); 
-$date2 = date('Y-m-d',$date1);
+$bb_no = $_POST['username1'];
+$date1= date_create($_POST['date1']); 
+$hours=$_POST['hours']; 
+$date2= date_create($_POST['date2']); 
+
 $remarks= $_POST['remarks']; 
 
-    
+$diff=date_diff($date1,$date2);
+$days=$diff->format("%a");
+$day =  $date1->format("w");
+
+$date1= date_format($date1,'Y-m-d');
+$date2= date_format($date2,'Y-m-d');
+
+
 	if (empty($_POST["type"])) {
 	  echo  "Please enter your leave type <br>";
 	  $status ="NOTOK"; } 
-	if (empty($_POST["duration"])) {
-	  echo  "Duration is required. <br>";
-	  $status ="NOTOK";} 
-	else 
-            if(strlen($duration)>3)
+	if(strlen($days)>3)
             {
              echo "Invalid Duration. <br>";
    	         $status= "NOTOK";
@@ -87,27 +115,65 @@ $remarks= $_POST['remarks'];
 	echo "Start Date is required. <br>";
 	$status ="NOTOK";}
 	else
-	  if($date2 >date("Y-m-d")) {echo "Future date is not acceptable"; 
+	  if($date1 >date("Y-m-d")) {echo "Correct Start Date. <br>"; 
 	  $status ="NOTOK";} 
-	
-		
+	if (empty($_POST["date2"])) {
+	echo "End Date is required. <br>";
+	$status ="NOTOK";}
+	else
+	  if($date2 >date("Y-m-d")) {echo "Future date is not acceptable <br>"; 
+	  $status ="NOTOK";}   
+	if(empty($_POST["hours"])) {
+	echo "Hours is required. <br>";
+	$status ="NOTOK";}  
 
-		
-if($status !="NOTOK") {
-    $query = "INSERT INTO leave_data(BB_No, Type, 
-                    Start_Date, Duration, Remarks) VALUES ('$bb_no',
-                    '$type', '$date2', '$duration', '$remarks')";
+if($hours==4)
+{
+   $hours1=$hours;
+}
+else {
+	
+   if($hours%8===0)
+   {
+	   $hours1=8;
+   }	
+   else {
+	   echo "Hours is not correct <br>";
+	   $status ="NOTOK";}
+}		
+if($status !="NOTOK")
+{
+while($days >=0)
+			{
+			   if($day == 6 || $day == 0)
+			   {
+				  
+			   }
+			   else {
+				   
+				   $query = "INSERT INTO leave_data(BB_No, Type, 
+                    Start_Date, End_Date, Hours, Remarks) VALUES ('$bb_no',
+                    '$type', '$date1', '$date1', '$hours1','$remarks')";
+					mysqli_query($con, $query);
 					
 					
-	
-
-
-    if(mysqli_query($con, $query)) 
-		{
+					   
+			   } 
+			   $date1=date('Y-m-d', strtotime($date1. ' + 1 days'));
+			   $date1= date_create($date1);
+			   $day =  $date1->format("w");
+			   
+			   $days=$days-1;
+			   $date1= date_format($date1,'Y-m-d'); 
+			   
+			} 
+			
        		      echo  "Leave has been recorded"; 
-		}
-   }
+		     
+
+   
  
+  } 
 }
 
 mysqli_close($con);
--- proj/view_monthly.php
+++ proj/view_monthly.php
@@ -22,10 +22,29 @@ echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
 
 
-$result = $con->query("SELECT BB_No,Type,Start_Date,End_Date,SUM(Hours) AS LEAVE_HOURS FROM leave_data WHERE Start_Date >= '$obj->sdate' 
-                      AND End_Date <='$obj->edate' GROUP BY BB_No, Type");
+$result = $con->query("SELECT Type,Start_Date,End_Date,SUM(Hours) AS TOTAL FROM leave_data WHERE Start_Date >= '$obj->sdate' 
+                      AND End_Date <='$obj->edate' GROUP BY Start_Date, Type");
+					  
 
-
+if($result->num_rows>0)
+{
+	while($row = $result->fetch_assoc()) {
+            $curr_date=$row["Start_Date"];
+			$date1=date_create($row["Start_Date"]);
+			$date2=date_create($row["End_Date"]);
+			$diff=date_diff($date1,$date2);
+			$days=$diff->format("%a");
+			
+			while($days >=0)
+			{
+			   
+				   echo json_encode(" Type: " . $row["Type"]. " Start_Date : " . $curr_date. " End_Date : " . $curr_date. " Leave Hours : " . $row["TOTAL"]);
+			   $curr_date=date('Y-m-d', strtotime($row["Start_Date"]. ' + 1 days'));
+			   $days=$days-1;   
+			   
+			}
+ }
+} 
 $outp = array();
 
 
@@ -35,11 +54,12 @@ $outp = $result->fetch_all(MYSQLI_ASSOC);
 echo json_encode($outp);
 
 
+
+
 mysqli_close($con);
 
 
 ?>
-
 
 
 
